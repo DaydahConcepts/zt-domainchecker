099267ca1de498ad948ff734a137b33da3378bb1
Viet Vu <jooservices@gmail.com>
2015-03-27 14:10:50 +0700

    ^ code clean up

--- mod_ztdomainchecker.php
+++ mod_ztdomainchecker.php
@@ -1,3 +1,17 @@
 <?php
 
+/**
+ * ZT Domain checker
+ * 
+ * @package     Joomla
+ * @subpackage  Module
+ * @version     1.0.0
+ * @author      ZooTemplate 
+ * @email       support@zootemplate.com 
+ * @link        http://www.zootemplate.com 
+ * @copyright   Copyright (c) 2015 ZooTemplate
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die('Restricted access');
+
 require JModuleHelper::getLayoutPath('mod_ztdomainchecker', $params->get('layout', 'default'));
